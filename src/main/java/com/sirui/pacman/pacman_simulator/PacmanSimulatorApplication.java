6e2a3a2b99b37b52cb368f5dfc89246130cd3f76
Sirui Yan <1511374803@qq.com>
2019-03-02 14:24:23 +1100

    create project

---
+++ src/main/java/com/sirui/pacman/pacman_simulator/PacmanSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.sirui.pacman.pacman_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmanSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmanSimulatorApplication.class, args);
+	}
+
+}
